db6067d75f07d106232db3cdde4c94141aaf66e4
HowManySmall <awesomedude5475@gmail.com>
2025-03-07 23:25:35 -0700

    update build scripts and add release configuration

--- src/plugin.storybook.ts
+++ src/plugin.storybook.ts
@@ -6,5 +6,5 @@ import type { Storybook } from "@rbxts/ui-labs";
 export = {
 	groupRoots: true,
 	name: "Plugin",
-	storyRoots: [script.Parent!.FindFirstChild("stories")!],
+	storyRoots: [script.Parent!.FindFirstChild("__stories__")!],
 } satisfies Storybook;
